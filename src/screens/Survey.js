918208b798d54e7156a681db244d6bb86fab6f29
jrlmc-iscteiul <jrlmc@iscte-iul.pt>
2021-01-31 23:44:32 +0000

    survey ainda nao está

--- src/screens/Survey.js
+++ src/screens/Survey.js
@@ -11,205 +11,191 @@ import {
 } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
 
-class Survey extends React.Component {
-  state = {
-    age: null,
-    hairType: null,
-    hasColouredHair: null,
-    hairColour: null,
-    numberWashes: null,
-    livingPlace: null,
-    useThermalProducts: null,
-    desiredHair: null,
-  };
+const Survey = ({navigation, errorMessage, onSubmit}) => {
+  const {state, survey, clearErrorMessage} = useContext(AuthContext);
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ImageBackground
-          style={styles.backgroundImage}
-          source={require('../Imagens/marble.jpg')}>
-          <Image
-            style={styles.menuIcon}
-            source={require('../Imagens/menuIcon.png')}
-          />
-          <Text style={styles.text}>Inquérito Inicial</Text>
-          <Text style={styles.subText}>Vamos começar!</Text>
-        </ImageBackground>
+  const [age, setAge] = useState('');
+  const [hairType, setHairType] = useState('');
+  const [hasColouredHair, setHasColouredHair] = useState('');
+  const [hairColour, setHairColour] = useState('');
+  const [numberWashes, setNumberWashes] = useState('');
+  const [livingPlace, setLivingPlace] = useState('');
+  const [useHeatTools, setUseHeatTools] = useState('');
+  const [useThermalProducts, setUseThermalProducts] = useState('');
+  const [desiredHair, setDesiredHair] = useState('');
 
-        <ScrollView>
-          <View style={styles.view}>
-            <Spacer />
-            <Text style={styles.questionsText}>1. Quantos anos tem?</Text>
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require('../Imagens/marble.jpg')}>
+        <Image
+          style={styles.menuIcon}
+          source={require('../Imagens/menuIcon.png')}
+        />
+        <Text style={styles.text}>Inquérito Inicial</Text>
+        <Text style={styles.subText}>Vamos começar!</Text>
+      </ImageBackground>
 
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.age}
-                //itemStyle={{fontFamily: 'Roboto', fontSize: 13}}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({age: itemValue})
-                }>
-                <Picker.Item label="Menos de 18 anos" value="under18" />
-                <Picker.Item label="18-25 anos" value="18-25" />
-                <Picker.Item label="26-35 anos" value="26-35" />
-                <Picker.Item label="36-45 anos" value="36-45" />
-                <Picker.Item label="46-55 anos" value="46-55" />
-                <Picker.Item label="56-65 anos" value="56-65" />
-                <Picker.Item label="Acima de 65 anos" value="over65" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>
-              2. Qual é o seu tipo de cabelo?
-            </Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.hairType}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({hairType: itemValue})
-                }>
-                <Picker.Item label="Liso" value="liso" />
-                <Picker.Item label="Ondulado" value="ondulado" />
-                <Picker.Item label="Encaracolado" value="encaracolado" />
-                <Picker.Item label="Frisado" value="frisado" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>3. Tem cabelo pintado?</Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.hasColouredHair}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({hasColouredHair: itemValue})
-                }>
-                <Picker.Item label="Sim" value="yes" />
-                <Picker.Item label="Não" value="no" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>
-              4. Qual a sua cor de cabelo natural?
-            </Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.hairColour}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({hairColour: itemValue})
-                }>
-                <Picker.Item label="Preto" value="preto" />
-                <Picker.Item label="Castanho" value="castanho" />
-                <Picker.Item label="Loiro" value="loiro" />
-                <Picker.Item label="Ruivo" value="ruivo" />
-                <Picker.Item label="Grisalho" value="grisalho" />
-                <Picker.Item label="Outra cor" value="outra cor" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>
-              5. Quantas vezes lava o cabelo por semana?
-            </Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.numberWashes}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({numberWashes: itemValue})
-                }>
-                <Picker.Item label="1x por semana" value="1x" />
-                <Picker.Item label="2-3x por semana" value="2-3x" />
-                <Picker.Item label="Dia sim dia não" value="diaSimDiaNao" />
-                <Picker.Item label="Todos os dias" value="everyday" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>6. Onde vive?</Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.livingPlace}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({livingPlace: itemValue})
-                }>
-                <Picker.Item label="Centro urbano" value="centroUrbano" />
-                <Picker.Item label="Arredores" value="arredores" />
-                <Picker.Item label="Campo" value="campo" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>
-              7. Usa ferramentas de calor?
-            </Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.seventhQuestion}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({seventhQuestion: itemValue})
-                }>
-                <Picker.Item label="Sim" value="yes" />
-                <Picker.Item label="Não" value="no" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>8. Usa produtos térmicos?</Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.useThermalProducts}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({useThermalProducts: itemValue})
-                }>
-                <Picker.Item label="Sim" value="yes" />
-                <Picker.Item label="Não" value="no" />
-              </Picker>
-            </View>
-            <Spacer />
-
-            <Text style={styles.questionsText}>
-              9. Como gostava que o seu cabelo fosse?
-            </Text>
-
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={this.state.desiredHair}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({desiredHair: itemValue})
-                }>
-                <Picker.Item label="Longo" value="longo" />
-                <Picker.Item label="Liso" value="liso" />
-                <Picker.Item label="Caracóis Perfeitos" value="caracois" />
-                <Picker.Item label="Mais brilhante" value="brilho" />
-                <Picker.Item label="Mais denso" value="denso" />
-                <Picker.Item label="Mais volume" value="volume" />
-              </Picker>
-            </View>
-
-            <Spacer />
-
-            <Button
-              style={styles.button}
-              color="#5A5757"
-              title="Pronto"
-              onPress={() => this.props.navigation.navigate('Perfil')}
-            />
+      <ScrollView>
+        <View style={styles.view}>
+          <Spacer />
+          <Text style={styles.questionsText}>1. Quantos anos tem?</Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={age}
+              //itemStyle={{fontFamily: 'Roboto', fontSize: 13}}
+              onValueChange={(itemValue) => setAge(itemValue)}>
+              <Picker.Item label="Menos de 18 anos" value="under18" />
+              <Picker.Item label="18-25 anos" value="18-25" />
+              <Picker.Item label="26-35 anos" value="26-35" />
+              <Picker.Item label="36-45 anos" value="36-45" />
+              <Picker.Item label="46-55 anos" value="46-55" />
+              <Picker.Item label="56-65 anos" value="56-65" />
+              <Picker.Item label="Acima de 65 anos" value="over65" />
+            </Picker>
           </View>
           <Spacer />
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
-}
+
+          <Text style={styles.questionsText}>
+            2. Qual é o seu tipo de cabelo?
+          </Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={hairType}
+              onValueChange={(itemValue) => setHairType(itemValue)}>
+              <Picker.Item label="Liso" value="liso" />
+              <Picker.Item label="Ondulado" value="ondulado" />
+              <Picker.Item label="Encaracolado" value="encaracolado" />
+              <Picker.Item label="Frisado" value="frisado" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>3. Tem cabelo pintado?</Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={hasColouredHair}
+              onValueChange={(itemValue) => setHasColouredHair(itemValue)}>
+              <Picker.Item label="Sim" value="yes" />
+              <Picker.Item label="Não" value="no" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>
+            4. Qual a sua cor de cabelo natural?
+          </Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={hairColour}
+              onValueChange={(itemValue) => setHairColour(itemValue)}>
+              <Picker.Item label="Preto" value="preto" />
+              <Picker.Item label="Castanho" value="castanho" />
+              <Picker.Item label="Loiro" value="loiro" />
+              <Picker.Item label="Ruivo" value="ruivo" />
+              <Picker.Item label="Grisalho" value="grisalho" />
+              <Picker.Item label="Outra cor" value="outra cor" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>
+            5. Quantas vezes lava o cabelo por semana?
+          </Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={numberWashes}
+              onValueChange={(itemValue) => setNumberWashes(itemValue)}>
+              <Picker.Item label="1x por semana" value="1x" />
+              <Picker.Item label="2-3x por semana" value="2-3x" />
+              <Picker.Item label="Dia sim dia não" value="diaSimDiaNao" />
+              <Picker.Item label="Todos os dias" value="everyday" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>6. Onde vive?</Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={livingPlace}
+              onValueChange={(itemValue) => setLivingPlace(itemValue)}>
+              <Picker.Item label="Centro urbano" value="centroUrbano" />
+              <Picker.Item label="Arredores" value="arredores" />
+              <Picker.Item label="Campo" value="campo" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>7. Usa ferramentas de calor?</Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={useHeatTools}
+              onValueChange={(itemValue) => setUseHeatTools(itemValue)}>
+              <Picker.Item label="Sim" value="yes" />
+              <Picker.Item label="Não" value="no" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>8. Usa produtos térmicos?</Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={useThermalProducts}
+              onValueChange={(itemValue) => setUseThermalProducts(itemValue)}>
+              <Picker.Item label="Sim" value="yes" />
+              <Picker.Item label="Não" value="no" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Text style={styles.questionsText}>
+            9. Como gostava que o seu cabelo fosse?
+          </Text>
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={desiredHair}
+              onValueChange={(itemValue) => setDesiredHair(itemValue)}>
+              <Picker.Item label="Longo" value="longo" />
+              <Picker.Item label="Liso" value="liso" />
+              <Picker.Item label="Caracóis Perfeitos" value="caracois" />
+              <Picker.Item label="Mais brilhante" value="brilho" />
+              <Picker.Item label="Mais denso" value="denso" />
+              <Picker.Item label="Mais volume" value="volume" />
+            </Picker>
+          </View>
+          <Spacer />
+
+          <Button
+            style={styles.button}
+            color="#5A5757"
+            title="Pronto"
+            onPress={() =>
+              onSubmit({
+                age,
+                hairType,
+                hairColour,
+                hasColouredHair,
+                numberWashes,
+                livingPlace,
+                useHeatTools,
+                useThermalProducts,
+                desiredHair,
+              })
+            }
+          />
+
+          {errorMessage ? (
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
+          ) : null}
+        </View>
+        <Spacer />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 Survey.navigationOptions = () => {
   return {
