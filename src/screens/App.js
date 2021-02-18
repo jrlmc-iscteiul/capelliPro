0ab5badac9e708f1f1903aff4063f037f45b7dc5
aplcg-iscteiul <aplcg@iscte-iul.pt>
2021-02-18 18:59:37 +0000

    1

--- src/screens/App.js
+++ src/screens/App.js
@@ -33,13 +33,13 @@ import {Provider as AuthProvider} from '../context/AuthContext';
 import {setNavigator} from '../navigationRef';
 
 const switchNavigator = createSwitchNavigator({
-  ResolveAuth: ResolveAuthScreen,
+  /* ResolveAuth: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     getStarted: GetStarted,
     Signup: RegisterFrame,
     Signin: LoginFrame,
     Survey: Survey,
-  }),
+  }), */
   mainFlow: createDrawerNavigator(
     {
       Perfil: {
